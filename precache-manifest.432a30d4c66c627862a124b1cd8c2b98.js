195e1321273c87328e5ec8183308d71c6f24bac1
knightdave <13086223+knightdave@users.noreply.github.com>
2020-07-22 14:13:20 +0200

    Updates

---
+++ precache-manifest.432a30d4c66c627862a124b1cd8c2b98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b90b9ec614d345e3877bf338eae5026",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8b72f97031c45b00b144",
+    "url": "/monsters-rolodex/static/css/main.43dd6c04.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b72f97031c45b00b144",
+    "url": "/monsters-rolodex/static/js/main.403b7706.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
